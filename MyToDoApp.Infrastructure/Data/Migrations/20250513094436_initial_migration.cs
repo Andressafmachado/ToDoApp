77e9ed0c4bdabf06a2349c03a22d978a54502f7f
andressaaihr <135823951+andressaaihr@users.noreply.github.com>
2025-08-05 11:55:33 +0200

    revert latest changes

---
+++ MyToDoApp.Infrastructure/Data/Migrations/20250513094436_initial_migration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MyToDoApp.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial_migration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: false),
+                    Priority = table.Column<int>(type: "integer", nullable: false),
+                    ParentToDoId = table.Column<int>(type: "integer", nullable: true),
+                    Auth0UserId = table.Column<string>(type: "text", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ToDos_ToDos_ParentToDoId",
+                        column: x => x.ParentToDoId,
+                        principalTable: "ToDos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_ParentToDoId",
+                table: "ToDos",
+                column: "ParentToDoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_Priority_Auth0UserId",
+                table: "ToDos",
+                columns: new[] { "Priority", "Auth0UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDos");
+        }
+    }
+}
---
+++ MyToDoApp.Infrastructure/Data/Migrations/20250623194052_added_composite_index.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyToDoApp.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_composite_index : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_ParentToDoId_Priority_Auth0UserId",
+                table: "ToDos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_Priority_Auth0UserId",
+                table: "ToDos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_ParentToDoId_Priority_Auth0UserId",
+                table: "ToDos",
+                columns: new[] { "ParentToDoId", "Priority", "Auth0UserId" },
+                unique: true,
+                filter: "\"ParentToDoId\" IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_Priority_Auth0UserId",
+                table: "ToDos",
+                columns: new[] { "Priority", "Auth0UserId" },
+                unique: true,
+                filter: "\"ParentToDoId\" IS NULL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_ParentToDoId_Priority_Auth0UserId",
+                table: "ToDos");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ToDos_Priority_Auth0UserId",
+                table: "ToDos");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_ParentToDoId_Priority_Auth0UserId",
+                table: "ToDos",
+                columns: new[] { "ParentToDoId", "Priority", "Auth0UserId" },
+                unique: true,
+                filter: "[ParentId] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ToDos_Priority_Auth0UserId",
+                table: "ToDos",
+                columns: new[] { "Priority", "Auth0UserId" },
+                unique: true,
+                filter: "[ParentId] IS NULL");
+        }
+    }
+}
